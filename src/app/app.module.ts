d813b3b786f0ed55748e31b9fde3999e51512372
Benjamin Cook <Ben.cook@fcsamerica.com>
2023-05-26 15:45:21 -0500

    Module 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   // define which component belongs to this module
   declarations: [ 
     AppComponent,
     ProductListComponent,
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe,
+    StarComponent
   ],
   // define the external modules that we want available to all components that belong to this module
   imports: [ 
